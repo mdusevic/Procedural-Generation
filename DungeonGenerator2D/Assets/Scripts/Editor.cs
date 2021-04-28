29e1182f58ee649d6653b55f006d8c24146214a8
Mara Dusevic <s200494@students.aie.edu.au>
2021-04-28 16:53:26 +1000

    Added procedural generation project with tile spawning, added sprite

---
+++ DungeonGenerator2D/Assets/Scripts/Editor.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Editor : EditorWindow
+{
+    private bool optionalEnabled;
+    private bool voidGenTriggered = false;
+    private bool roomGenTriggered = false;
+    private float testFloat;
+    private Vector2 mapLimits;
+
+    private MapGenerator GenManager;
+
+    // Adds menu item to the window
+    [MenuItem ("Window/Dungeon Generator")]
+
+    // Shows existing window instance. If one doesn't exist, make one.
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(Editor));
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+
+        GenManager = (MapGenerator)EditorGUILayout.ObjectField(new GUIContent("Map Generation Manager", "The object in scene that contains the MapGeneration script."), GenManager, typeof(MapGenerator), true);
+
+        GUILayout.Label("Generation", EditorStyles.boldLabel);
+
+        GUILayout.BeginHorizontal();
+
+        // Button to trigger room generation
+        if (GUILayout.Button(new GUIContent("Generate Void Space", "Randomly places tiles to create void space.")))
+        {
+            Debug.Log("GENERATING VOID SPACE");
+
+            voidGenTriggered = true;
+            GenManager.CreateVoidTiles();
+        }
+
+        if (voidGenTriggered)
+        {
+            // Button to trigger room generation
+            if (GUILayout.Button(new GUIContent("Generate Rooms", "Randomly places rooms on the map.")) && voidGenTriggered)
+            {
+                Debug.Log("GENERATING ROOMS");
+
+                roomGenTriggered = true;
+                GenManager.GenerateRooms();
+            }
+        }
+
+        if (roomGenTriggered)
+        {
+            // Button to trigger corridor generation
+            if (GUILayout.Button(new GUIContent("Generate Corridors", "Randomly places corridor on the map. NOTE: Generate after rooms have been created.")))
+            {
+                Debug.Log("GENERATING CORRIDORS");
+
+                GenManager.GenerateCorridors();
+            }
+        }
+
+        GUILayout.EndHorizontal();
+
+        mapLimits = EditorGUILayout.Vector2Field(new GUIContent("Map Limits", "Changes the size of the generated map"), mapLimits);
+        GenManager.SetMapSize(mapLimits);
+
+        // Button to reset map
+        if (GUILayout.Button(new GUIContent("Reset", "Removes all components of map")))
+        {
+            Debug.Log("RESET MAP");
+
+            voidGenTriggered = false;
+            roomGenTriggered = false;
+            GenManager.ResetMap();
+        }
+
+        optionalEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", optionalEnabled);
+        testFloat = EditorGUILayout.Slider("Slider", testFloat, -3, 3);
+        EditorGUILayout.EndToggleGroup();
+    }
+}
---
+++ DungeonGenerator2D/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGenerator : MonoBehaviour
+{
+    #region Settings
+
+    [Header("Base Settings")]
+
+    [SerializeField]
+    [Tooltip("Tilemap used to spawn tiles and map")]
+    private Tilemap tilemap;
+
+    [SerializeField]
+    [Tooltip("Sets the size of the map")]
+    private BoundsInt mapSize;
+
+    #endregion
+
+    #region Tiles
+
+    [Space(4)]
+    [Header("Tiles")]
+
+    [SerializeField]
+    [Tooltip("Textures used to fill the map's void spaces")]
+    private Tile[] voidTiles;
+
+    #endregion
+
+    #region Rooms
+
+    [Space(4)]
+    [Header("Rooms")]
+
+    [SerializeField]
+    [Tooltip("All room prefabs to be spawned")]
+    private GameObject[] rooms;
+
+    #endregion
+
+    public void SetMapSize(Vector2 newMapSize)
+    {
+        mapSize.x = (int)newMapSize.x;
+        mapSize.y = (int)newMapSize.y;
+    }
+
+    public void CreateVoidTiles()
+    {
+        ResetMap();
+
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                int tileID = Random.Range(0, voidTiles.Length);
+
+                tilemap.SetTile(new Vector3Int(-x + mapSize.x / 2, -y + mapSize.y / 2, 0), voidTiles[tileID]);
+            }
+        }
+    }
+
+    public void GenerateRooms()
+    {
+
+    }
+
+    public void GenerateCorridors()
+    {
+
+    }
+
+    private void GenerateDoors()
+    {
+
+    }
+
+    public void ResetMap()
+    {
+        tilemap.ClearAllTiles();
+    }
+}
